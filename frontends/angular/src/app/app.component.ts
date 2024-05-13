77439f4fc1ebbb2fbcf7c6943b20cd7aa803dae9
Milan <kmilanm13@gmail.com>
2024-05-13 17:06:16 +0100

    [Milan] Generated Navigation Bar component

--- frontends/angular/src/app/app.component.ts
+++ frontends/angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
