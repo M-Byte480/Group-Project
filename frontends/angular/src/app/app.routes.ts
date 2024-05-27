078e76279eeba2c2af27f59660943f70ca1fd00d
Milan <kmilanm13@gmail.com>
2024-05-27 23:50:12 +0100

    [Milan] Created some html and played with tailwind

--- frontends/angular/src/app/app.routes.ts
+++ frontends/angular/src/app/app.routes.ts
@@ -1,7 +1,11 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
+import { Routes } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+import { ProfileComponent } from './components/profile/profile.component'
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-];
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'profile', component: ProfileComponent }, // todo: add auth guard
+    { path: 'login', component: HomeComponent }, // todo: add login component
+    { path: '**', redirectTo: '/home' }, // catch all route
+]
