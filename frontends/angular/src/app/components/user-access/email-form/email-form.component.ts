37992939c79092d474a800f8c37aa18933f0ba3c
Milan <kmilanm13@gmail.com>
2024-05-31 03:53:37 +0100

    [Milan] Added Remember me and registration button

--- frontends/angular/src/app/components/user-access/email-form/email-form.component.ts
+++ frontends/angular/src/app/components/user-access/email-form/email-form.component.ts
@@ -10,6 +10,7 @@ import { MatInput } from '@angular/material/input';
 import { NgIf } from '@angular/common';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-email-form',
@@ -23,6 +24,7 @@ import { MatIcon } from '@angular/material/icon';
         MatIconButton,
         MatIcon,
         MatButton,
+        MatSlideToggle,
     ],
     templateUrl: './email-form.component.html',
     styleUrl: './email-form.component.css',
